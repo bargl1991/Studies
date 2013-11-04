255e8617a6339e39b0b2a9fc7088aabd13447fd9
pkruk <piotr@kruk.co>
2013-11-04 07:18:48 +0100

    PD z PSI

---
+++ PSI/cw2_pd/zad1/script.js
@@ -0,0 +1,39 @@
+/* http://github.com/enure/Hide-All-Except-One */
+
+(function($){
+  $.hideAllExcept = function(){
+    function init() {
+      var hash = window.location.hash;
+
+      // make it bookmarkable/refreshable. but, no history.
+      (!hash) ? hideShow('#' + $('#toggleThis > div:first').attr('id')) : hideShow(window.location.hash);
+
+      // add click handler.
+      $('a.toggle').click(function(e) {
+        e.preventDefault();
+        var href = $(this).attr('href');
+        hideShow(href);
+      });
+      $('#b1').click(function(e){
+        e.preventDefault();
+        alert("xD klikles mnie lol xD ")
+      });
+      $('#b2').click(function(e){
+        e.preventDefault();
+        e = document.getElementById('textelement');
+        tekscior(e);
+      });
+    }
+    function tekscior(e){
+      document.getElementById("textelement").innerHTML = "Nie tykaj mnie";
+    }
+
+    function hideShow(el) {
+      $(el).show().siblings().hide();
+
+      $('a.toggle').removeClass('active');
+      $('a[href="' + el + '"]').addClass('active');
+    }
+    init();
+  };
+})(jQuery);
